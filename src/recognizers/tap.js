d7cebf9ed40451aa30c0eadd446b8cfda31820fe
UltimateSupreme <ultimatesupreme2212@gmail.com>
2016-03-02 21:02:19 +0530

    Update tap.js
    ES6 export

--- src/recognizers/tap.js
+++ src/recognizers/tap.js
@@ -1,5 +1,5 @@
 /**
- * A tap is ecognized when the pointer is doing a small tap/click. Multiple taps are recognized if they occur
+ * A tap is recognized when the pointer is doing a small tap/click. Multiple taps are recognized if they occur
  * between the given interval and position. The delay option can be used to recognize multi-taps without firing
  * a single tap.
  *
@@ -8,7 +8,7 @@
  * @constructor
  * @extends Recognizer
  */
-function TapRecognizer() {
+export function TapRecognizer() {
     Recognizer.apply(this, arguments);
 
     // previous time and center,
